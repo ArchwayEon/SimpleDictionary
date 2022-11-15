eada81826cc9e5896239f6d63aa5ef68de441567
Jeff Roach <37549850+ArchwayEon@users.noreply.github.com>
2022-11-15 04:20:11 -0500

    Minor formatting

--- SimpleDictionary/Controllers/HomeController.cs
+++ SimpleDictionary/Controllers/HomeController.cs
@@ -4,46 +4,45 @@ using SimpleDictionary.Models.Entities;
 using SimpleDictionary.Services;
 using System.Diagnostics;
 
-namespace SimpleDictionary.Controllers
+namespace SimpleDictionary.Controllers;
+
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    private readonly ILogger<HomeController> _logger;
+    private readonly IDictionaryRepository _dictionaryRepo;
+
+    public HomeController(IDictionaryRepository dictionaryRepo, ILogger<HomeController> logger)
     {
-        private readonly ILogger<HomeController> _logger;
-        private readonly IDictionaryRepository _dictionaryRepo;
+        _logger = logger;
+        _dictionaryRepo = dictionaryRepo;
+    }
 
-        public HomeController(IDictionaryRepository dictionaryRepo, ILogger<HomeController> logger)
-        {
-            _logger = logger;
-            _dictionaryRepo = dictionaryRepo;
-        }
+    public IActionResult Index()
+    {
+        var model = _dictionaryRepo.ReadAll();
+        return View(model);
+    }
 
-        public IActionResult Index()
-        {
-            var model = _dictionaryRepo.ReadAll();
-            return View(model);
-        }
+    public IActionResult AddWord()
+    {
+        return View();
+    }
 
-        public IActionResult AddWord()
-        {
-            return View();
-        }
+    [HttpPost]
+    public IActionResult AddWord(DictionaryEntry entry)
+    {
+        _dictionaryRepo.Create(entry);
+        return RedirectToAction("Index");
+    }
 
-        [HttpPost]
-        public IActionResult AddWord(DictionaryEntry entry)
-        {
-            _dictionaryRepo.Create(entry);
-            return RedirectToAction("Index");
-        }
+    public IActionResult Privacy()
+    {
+        return View();
+    }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
